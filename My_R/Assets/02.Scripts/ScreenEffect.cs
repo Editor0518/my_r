da1ed8866d8cc960974bf97663cec5af32522911
편오 <your5marimo@gmail.com>
2024-08-17 01:00:12 +0900

    배경추가

--- My_R/Assets/02.Scripts/ScreenEffect.cs
+++ My_R/Assets/02.Scripts/ScreenEffect.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ScreenEffect : MonoBehaviour
@@ -8,29 +7,34 @@ public class ScreenEffect : MonoBehaviour
 
     //진동
 
-    public void StartVibrate() {
-        Handheld.Vibrate();
+    public void StartVibrate()
+    {
+        //Handheld.Vibrate();
         Debug.Log("진동");
     }
 
     //화면 흔들림
 
-    public void Flinch() {
+    public void Flinch()
+    {
         ScreenShake(0.15f, 0, 0.1f);
     }
 
-    public void ScreenShake(float duration, float magnitudeX, float magnitudeY) {
+    public void ScreenShake(float duration, float magnitudeX, float magnitudeY)
+    {
         StartCoroutine(Shake(duration, magnitudeX, magnitudeY));
         Debug.Log("흔들기");
     }
 
-    IEnumerator Shake(float duration, float magnitudeX, float magnitudeY) {
+    IEnumerator Shake(float duration, float magnitudeX, float magnitudeY)
+    {
         Vector3 originalPos = cam.transform.localPosition;
 
         float elapsed = 0.0f;
 
-        while (elapsed < duration) {
-            float x = Random.Range(-1f, 1f) * magnitudeX;    
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitudeX;
             float y = Random.Range(-1f, 1f) * magnitudeY;
 
             cam.transform.localPosition = new Vector3(x, y, originalPos.z);
@@ -51,8 +55,9 @@ public class ScreenEffect : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.O)) {
-            
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+
         }
     }
 }
